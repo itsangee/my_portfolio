b98300b9eeb460852a4faa6aa56de7c21ad66f61
angelee otic <otic.angeleemae@gmail.com>
2023-06-16 21:36:17 +0100

    homepage plus image

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # get 'homepage/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'homepage/home'
+  # root to: 'homepage/home'
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'home', to: 'homepage#home'
 end
