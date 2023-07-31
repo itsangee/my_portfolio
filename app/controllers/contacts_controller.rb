357a2b5275786a730551f325e4da793382528c91
angelee otic <otic.angeleemae@gmail.com>
2023-07-31 11:17:39 +0100

    email contact form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,15 @@
+class ContactsController < ApplicationController
+  def create
+    @contact = Contact.new
+    @contact.create(contact_params)
+    if @contact.deliver
+      render json: { message: 'Email sent successfully' }
+    else
+      render json: @contact.errors
+    end
+  end
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
+end
